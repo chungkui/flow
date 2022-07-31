2bf4e3865aea648fb3224b34f809b81d6d0c6ee0
jason <35212@163.com>
2022-07-31 22:42:05 +0800

    项目精简

---
+++ flow-designer/node/start.js
@@ -0,0 +1,46 @@
+
+class StartModel extends CircleNodeModel {
+  static extendKey = 'StartModel';
+  constructor (data, graphModel) {
+    if (!data.text) {
+      data.text = ''
+    }
+    if (data.text && typeof data.text === 'string') {
+      data.text = {
+        value: data.text,
+        x: data.x,
+        y: data.y + 40
+      }
+    }
+    super(data, graphModel)
+  }
+
+  setAttributes () {
+    this.r = 18
+  }
+
+  getConnectedTargetRules () {
+    const rules = super.getConnectedTargetRules()
+    const notAsTarget = {
+      message: '起始节点不能作为边的终点',
+      validate: () => false
+    }
+    rules.push(notAsTarget)
+    return rules
+  }
+
+  getNodeStyle () {
+    const style = super.getNodeStyle()
+    return style
+  }
+}
+
+class StartView extends CircleNode {
+  static extendKey = 'StartNode';
+}
+
+const Start = {
+  type: 'snaker:start',
+  view: StartView,
+  model: StartModel
+}
